8136e8d56fae5f34adc0f2d3aebb8cd910a93e90
Josh8903 <thorngren87@gmail.com>
2020-09-23 19:58:35 -0500

    added parameter checks

--- script.js
+++ script.js
@@ -2,25 +2,35 @@ function generate() {
     var password = "";
     var chars = "";
     var length = prompt("Password length:", "8-128");
-    var lower = confirm("Use lowercase letters?");
-    if (lower) {
-        var chars = "abcdefghijklmnopqrstuvwxyz";
-    }
-    var upper = confirm("Use uppercase letters?");
-    if (upper) {
-        var chars = chars + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
-    var numbers = confirm("Use numbers?");
-    if (numbers) {
-        chars = chars + "0123456789";
-    }
-    var special = confirm("Use special characters?");
-    if (special) {
-        chars = chars + "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-    }
-    for (i = 0; i < length; i++) {
-        var c = Math.floor(Math.random() * chars.length + 1);
-        password += chars.charAt(c);
-        document.getElementById('password').value = password;
+    if (length < 8 || length > 128 || isNaN(length)) {
+        alert("You must enter a number between 8-128");
+        generate();
+    } else {
+        var lower = confirm("Use lowercase letters?");
+        if (lower) {
+            chars = "abcdefghijklmnopqrstuvwxyz";
+        }
+        var upper = confirm("Use uppercase letters?");
+        if (upper) {
+            chars = chars + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        var numbers = confirm("Use numbers?");
+        if (numbers) {
+            chars = chars + "0123456789";
+        }
+        var special = confirm("Use special characters?");
+        if (special) {
+            chars = chars + "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+        }
+        if (lower || upper || numbers || special) {
+            for (i = 0; i < length; i++) {
+                var c = Math.floor(Math.random() * chars.length + 1);
+                password += chars.charAt(c);
+                document.getElementById('password').value = password;
+            }
+        } else {
+            alert("You must select atleast one of the options.");
+            generate();
+        }
     }
 }
